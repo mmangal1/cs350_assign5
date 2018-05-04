279fe4ca5b020e8fe36ac0e81cfff6f4205c7eba
Ryan Sharpelletti <rsharpe1@binghamton.edu>
2018-05-03 21:43:48 -0400

    inode

--- newTry/inode.hpp
+++ newTry/inode.hpp
@@ -1,7 +1,12 @@
 #ifndef INODE_HPP
 #define INODE_HPP
+#include <string.h>
 
 typedef struct inode{
+	inode() {
+		memset(file_name, 0, sizeof file_name); 
+		memset(direct_ptrs, 0, sizeof direct_ptrs); 
+	}
 	char file_name[32];
 	int file_size;
 	int total_blocks;
